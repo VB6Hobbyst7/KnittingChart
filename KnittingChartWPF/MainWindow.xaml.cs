8dc73ce0bd26b2f5f2e0763265773569fbeda35c
Lindsay Alexander <linmalex@gmail.com>
2019-11-13 08:49:00 -0800

    create basic purl icon

--- KnittingChartWPF/MainWindow.xaml.cs
+++ KnittingChartWPF/MainWindow.xaml.cs
@@ -23,32 +23,13 @@ namespace KnittingChartWPF
         public MainWindow()
         {
             InitializeComponent();
-
-            //FileInfo fileInfo = new FileInfo(purlUri.AbsolutePath);
-            //FileInfo x = new FileInfo(@"C:\Users\linma\source\repos\KnittingChartWPF\icons\purl.svg");
-            //DirectoryInfo icons = new DirectoryInfo(@"C:\Users\linma\source\repos\KnittingChartWPF\icons\");
-            //var files = icons.GetFiles();
-            //FileInfo purlIcon = files[0];
-            //var exists = x.Exists;
-            //Grid grid = new Grid();
-
-            var h = this.ActualHeight;
-
-            // Create the Grid
-
             Grid dynamicGrid = new Grid();
-
             dynamicGrid.Width = 400;
             dynamicGrid.Height = Height;
-
             dynamicGrid.HorizontalAlignment = HorizontalAlignment.Center;
             dynamicGrid.VerticalAlignment = VerticalAlignment.Center;
-
             dynamicGrid.ShowGridLines = true;
-
             dynamicGrid.Background = new SolidColorBrush(Colors.LightSteelBlue);
-
-
 
             // create columns
             for (int i = 0; i < 3; i++)
@@ -56,93 +37,48 @@ namespace KnittingChartWPF
                 dynamicGrid.ColumnDefinitions.Add(new ColumnDefinition());
                 dynamicGrid.RowDefinitions.Add(new RowDefinition());
             }
-
-            RowDefinition x = dynamicGrid.RowDefinitions.Last();
-
-            var iconList = new List<Rectangle>();
-            for (int i = 0; i < 3; i++)
-            {
-                iconList.Add(IconBase(50));
-            }
-
-
-            DataGrid dataGrid = new DataGrid();
-            dataGrid.ItemsSource = iconList;
-            var drawing = PurlIcon();
-
-
-            DrawingGroup imageDrawings = new DrawingGroup();
-
-            // Create a 100 by 100 image with an upper-left point of (75,75). 
-
-
-
-            // Create a 25 by 25 image with an upper-left point of (0,150). 
-
-
-            //
-            // Use a DrawingImage and an Image control to
-            // display the drawings.
-            //
-            DrawingImage drawingImageSource = new DrawingImage(PurlIcon());
-
-            // Freeze the DrawingImage for performance benefits.
-            drawingImageSource.Freeze();
-
-            Image imageControl = new Image();
-            imageControl.Stretch = Stretch.None;
-            imageControl.Source = drawingImageSource;
-
-            // Create a border to contain the Image control.
-            Border imageBorder = new Border();
-            imageBorder.BorderBrush = Brushes.Gray;
-            imageBorder.BorderThickness = new Thickness(1);
-            imageBorder.HorizontalAlignment = HorizontalAlignment.Left;
-            imageBorder.VerticalAlignment = VerticalAlignment.Top;
-            imageBorder.Margin = new Thickness(20);
-            imageBorder.Child = imageControl;
-
-            this.Background = Brushes.White;
-            this.Margin = new Thickness(20);
-            this.Content = imageBorder;
-
         }
 
-        public Rectangle IconBase(double size)
+        public GeometryDrawing IconBase(double size)
         {
-            SolidColorBrush fillColor = new SolidColorBrush(Color.FromRgb(255, 255, 255));
-            SolidColorBrush strokeColor = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            GeometryDrawing rect = new GeometryDrawing(
+                new SolidColorBrush(Brushes.White.Color),
+                new Pen(Brushes.Black, 4),
+                new RectangleGeometry(new Rect(new Point(0, 0), new Point(size, size))
+            ));
 
-            var box = new Rectangle()
-            {
-                Fill = fillColor,
-                Stroke = strokeColor,
-                Height = size,
-                Width = size
-            };
-            return box;
+            return rect;
         }
 
         public DrawingGroup PurlIcon()
         {
-            GeometryDrawing rect = new GeometryDrawing(
-                    new SolidColorBrush(Brushes.White.Color),
-                    new Pen(Brushes.Black, 4),
-                    new RectangleGeometry(new Rect(new Point(0, 0), new Point(150, 150))
-                ));
+            DrawingGroup aDrawingGroup = new DrawingGroup();
+            aDrawingGroup.Children.Add(IconBase(150));
 
-            GeometryDrawing ellipseDrawing =
+            GeometryDrawing purlDot =
                 new GeometryDrawing(
                     new SolidColorBrush(Brushes.Black.Color),
                     new Pen(Brushes.Black, 4),
                     new EllipseGeometry(new Point(75, 75), 10, 10)
                 );
+            aDrawingGroup.Children.Add(purlDot);
 
-            // Create a DrawingGroup to contain the drawings.
-            DrawingGroup aDrawingGroup = new DrawingGroup();
-            aDrawingGroup.Children.Add(rect);
-            aDrawingGroup.Children.Add(ellipseDrawing);
             return aDrawingGroup;
+        }
+
+        public Image KnittingIcon()
+        {
+            DrawingImage drawingImageSource = new DrawingImage(PurlIcon());
+            // Freeze the DrawingImage for performance benefits.
+            drawingImageSource.Freeze();
+
+
+            Image imageControl = new Image()
+            {
+                Stretch = Stretch.None,
+                Source = drawingImageSource
+            };
+            return imageControl;
         }
     }
 }
